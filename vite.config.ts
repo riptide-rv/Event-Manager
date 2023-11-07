60b4c867027d73c07c10dc464e155cb10749b0af
Rohit Ramdas V <rohitramdas2003@gmail.com>
2023-11-08 01:50:29 +0530

    "deploy"

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
+    alias: {  
       "@": path.resolve(__dirname, "./src"),
     },
   },
